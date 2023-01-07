9f0067f48cfa15396064cd9e8a6ce44e2e49e60c
mrnpro <yegetanaty03@gmail.com>
2023-01-07 03:52:23 +0300

    ui change and add some logic

--- lib/Presentation/Pages/Splash/Components/splash_body.dart
+++ lib/Presentation/Pages/Splash/Components/splash_body.dart
@@ -1,11 +1,65 @@
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:telebirr/Presentation/Logics/cubit/splash_cubit.dart';
 
-class SplashBody extends StatelessWidget {
+import 'package:telebirr/Presentation/Pages/Onboarding/onboarding_screen.dart';
+
+import 'package:telebirr/Presentation/SizeConfig/size_config.dart';
+
+import '../../../CommonWidgets/TeleBirrLogo/telebirr_logo.dart';
+
+class SplashBody extends StatefulWidget {
   const SplashBody({super.key});
 
   @override
+  State<SplashBody> createState() => _SplashBodyState();
+}
+
+class _SplashBodyState extends State<SplashBody> {
+  late SplashCubit _splashCubit;
+  @override
+  void initState() {
+    _splashCubit = SplashCubit();
+    _splashCubit.startTimer();
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return BlocProvider.value(
+      value: _splashCubit,
+      child: BlocListener<SplashCubit, SplashState>(
+        listener: (context, state) {
+          if (state is ReadyToStartState) {
+            Navigator.pushNamed(context, OnBoardingScreen.route);
+          }
+        },
+        child: SizedBox(
+          width: SizeConfig.screenWidth,
+          height: SizeConfig.screenHeight,
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const SizedBox(),
+                Column(
+                  children: [
+                    const TeleBirrLogo(),
+                    addVerticalSpace(size: 40),
+                    const CircularProgressIndicator()
+                  ],
+                ),
+                Column(children: [
+                  const Text("from"),
+                  Image.asset("assets/images/cropedEthioTelLogo.png")
+                ])
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
