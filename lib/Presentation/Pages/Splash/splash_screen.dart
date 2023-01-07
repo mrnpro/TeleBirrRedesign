c7c87e4f6c3db2cc51dd568a904b07b95614e8d5
mrnpro <yegetanaty03@gmail.com>
2023-01-07 03:51:49 +0300

    size config initialized on splash

--- lib/Presentation/Pages/Splash/splash_screen.dart
+++ lib/Presentation/Pages/Splash/splash_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import '../../SizeConfig/size_config.dart';
 import 'Components/splash_body.dart';
 
 class SplashScreen extends StatelessWidget {
@@ -7,6 +8,8 @@ class SplashScreen extends StatelessWidget {
   static const String route = '/splashRoute';
   @override
   Widget build(BuildContext context) {
+    //
+    SizeConfig().init(context);
     return const Scaffold(
       body: SplashBody(),
     );
