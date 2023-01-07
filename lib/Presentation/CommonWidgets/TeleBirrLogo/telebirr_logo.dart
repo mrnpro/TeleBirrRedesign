2ce1c68a3cfa5d8ab22a218b9ef41ba03c1b2e91
mrnpro <yegetanaty03@gmail.com>
2023-01-07 03:49:29 +0300

    add telebirr logo to custom reusable widget

---
+++ lib/Presentation/CommonWidgets/TeleBirrLogo/telebirr_logo.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class TeleBirrLogo extends StatelessWidget {
+  const TeleBirrLogo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Image.asset(
+      "assets/images/telebirrlogo.png",
+    );
+  }
+}
