f95c31631f88a044fc8fc05a08874bcb3581b375
mrnpro <yegetanaty03@gmail.com>
2023-01-08 21:22:39 +0300

    update default button

--- lib/Presentation/CommonWidgets/DefaultButton/default_button.dart
+++ lib/Presentation/CommonWidgets/DefaultButton/default_button.dart
@@ -6,26 +6,34 @@ class DefaultButton extends StatelessWidget {
   const DefaultButton({
     Key? key,
     required this.btnTxt,
+    required this.onPressed,
   }) : super(key: key);
-
+  final VoidCallback onPressed;
   final String btnTxt;
   @override
   Widget build(BuildContext context) {
     ThemeData theme = Theme.of(context);
-    return TextButton(
-        style: TextButton.styleFrom(
-            backgroundColor: theme.primaryColor,
-            shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10))),
-        onPressed: () {},
-        child: SizedBox(
-            height: 42,
-            width: SizeConfig.screenWidth,
-            child: Center(
-              child: Text(
-                btnTxt,
-                style: theme.textTheme.button?.copyWith(color: Colors.white),
-              ),
-            )));
+    return Padding(
+      padding: const EdgeInsets.all(16),
+      child: SizedBox(
+        height: 42,
+        child: TextButton(
+            style: TextButton.styleFrom(
+                backgroundColor: theme.primaryColor,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10))),
+            onPressed: onPressed,
+            child: SizedBox(
+                height: 42,
+                width: SizeConfig.screenWidth,
+                child: Center(
+                  child: Text(
+                    btnTxt,
+                    style:
+                        theme.textTheme.button?.copyWith(color: Colors.white),
+                  ),
+                ))),
+      ),
+    );
   }
 }
