28471067c7438320aa896bc4c2434aa46a367c99
mrnpro <yegetanaty03@gmail.com>
2023-01-08 21:22:58 +0300

    create telebirr white logo

---
+++ lib/Presentation/CommonWidgets/TeleBirrLogo/telebirr_white_logo.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class TeleBirrWhiteLogo extends StatelessWidget {
+  const TeleBirrWhiteLogo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Image.asset("assets/images/telebirrWhiteLogo.png");
+  }
+}
