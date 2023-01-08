df2ddd915bf6fb271bba54312b57ace2e658a2c7
mrnpro <yegetanaty03@gmail.com>
2023-01-08 21:37:03 +0300

    add some routes

--- lib/Presentation/Routes/router.dart
+++ lib/Presentation/Routes/router.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:telebirr/Presentation/Pages/CreateAccount/create_account_screen.dart';
+import 'package:telebirr/Presentation/Pages/Home/home_screen.dart';
+import 'package:telebirr/Presentation/Pages/Login/login_screen.dart';
 import 'package:telebirr/Presentation/Pages/Onboarding/onboarding_screen.dart';
+import 'package:telebirr/Presentation/Pages/QuickCreate/quick_create_screen.dart';
 import 'package:telebirr/Presentation/Pages/Splash/splash_screen.dart';
 
 class GeneratedRoute {
@@ -9,7 +13,14 @@ class GeneratedRoute {
         return MaterialPageRoute(builder: (_) => const SplashScreen());
       case OnBoardingScreen.route:
         return MaterialPageRoute(builder: (_) => const OnBoardingScreen());
-
+      case LoginScreen.route:
+        return MaterialPageRoute(builder: (_) => const LoginScreen());
+      case CreateAccount.route:
+        return MaterialPageRoute(builder: (_) => const CreateAccount());
+      case QuickCreateScreen.route:
+        return MaterialPageRoute(builder: (_) => const QuickCreateScreen());
+      case HomeScreen.route:
+        return MaterialPageRoute(builder: (_) => const HomeScreen());
       default:
         return MaterialPageRoute(
             builder: (_) => const Center(
