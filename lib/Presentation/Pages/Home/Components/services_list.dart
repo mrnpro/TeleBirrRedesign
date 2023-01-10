1f90be1c7756c4700a57e6f630f5d612b5bd7166
mrnpro <yegetanaty03@gmail.com>
2023-01-10 15:36:52 +0300

    separate service card from list

--- lib/Presentation/Pages/Home/Components/services_list.dart
+++ lib/Presentation/Pages/Home/Components/services_list.dart
@@ -16,31 +16,55 @@ class ServicesList extends StatelessWidget {
     return Expanded(
       child: ListView.builder(
         itemCount: services.length,
-        itemBuilder: (context, index) => Padding(
-          padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 16),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Container(
-                decoration: BoxDecoration(
-                    color: theme.colorScheme.secondary,
-                    borderRadius: const BorderRadius.all(kRadius)),
-                height: 45,
-                width: 45,
-                child: Center(
-                    child: Image.asset(
-                  services[index].imgPath1,
-                )),
-              ),
-              Align(
+        itemBuilder: (context, index) =>
+            ServiceListCard(theme: theme, index: index, onPressed: () {}),
+      ),
+    );
+  }
+}
+
+class ServiceListCard extends StatelessWidget {
+  const ServiceListCard({
+    Key? key,
+    required this.theme,
+    required this.index,
+    required this.onPressed,
+  }) : super(key: key);
+
+  final ThemeData theme;
+  final int index;
+  final VoidCallback onPressed;
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 16, bottom: 16, right: 16),
+      child: InkWell(
+        onTap: onPressed,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                  color: theme.colorScheme.secondary,
+                  borderRadius: const BorderRadius.all(kRadius)),
+              height: 45,
+              width: 45,
+              child: Center(
+                  child: Image.asset(
+                services[index].imgPath1,
+              )),
+            ),
+            SizedBox(
+              width: 160,
+              child: Align(
                   alignment: Alignment.centerLeft,
                   child: Text(services[index].itemText,
                       style: theme.textTheme.subtitle1)),
-              const SizedBox(),
-              IconButton(
-                  onPressed: () {}, icon: const Icon(Icons.arrow_forward_ios))
-            ],
-          ),
+            ),
+            const SizedBox(),
+            IconButton(
+                onPressed: () {}, icon: const Icon(Icons.arrow_forward_ios))
+          ],
         ),
       ),
     );
